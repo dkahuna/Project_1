bbc1f04f531042cc0a8372447b4184bab4db595b
Khamhuang, David <khameye19888@gmail.com>
2019-06-15 13:27:59 -0500

    GPS locations

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,57 @@
 // Google's library call for the map
-var map;
+var map, infoWindow;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 36.1447034, lng: -86.8026551},
     zoom: 19
   });
+  var marker = new google.maps.Marker({
+    position: {lat: 36.145353, lng: -86.802771},
+    map: map,
+    title: "Recycling Zone",
+    animation: google.maps.Animation.Bounce,
+    draggable: true
+  });
+  
+  var marker01 = new google.maps.Marker({
+    position: {lat: 36.144912, lng: -86.803641},
+    map: map,
+    title: "Renewable",
+    animation: google.maps.Animation.Bounce,
+    draggable: true
+  });
+  infoWindow = new google.maps.InfoWindow;
+ 
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Location found.');
+      infoWindow.open(map);
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
 }
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your browser doesn\'t support geolocation.');
+  infoWindow.open(map);
+}
+
+
+
 
 $(function(){
 // NavBar Functions
@@ -37,6 +83,7 @@ function closeNav() {
   $(".map-launch").click(function() {
     $("#map").css("display","inherit");
     $(".map-launch").css("display", "none");
+    initMap();
   });
 
 // Open and Closing tab for user(Lines 43-49)
@@ -48,6 +95,5 @@ function closeNav() {
     closeNav();
   });
 
-
-
-  });
+  
+});
