0f1c1fe2c3f8d62f52dcf3aedb13b97e7f06280e
Enragedtoast <edwardsjtimothy@gmail.com>
2019-06-15 12:54:34 -0500

    added news

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,12 +12,10 @@ $(function(){
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
   document.getElementById("main").style.marginLeft = "250px";
-  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("main").style.marginLeft= "0";
-  document.body.style.backgroundColor = "white";
 }
   // Your web app's Firebase configuration
   var firebaseConfig = {
@@ -37,9 +35,70 @@ function closeNav() {
 
 // news API d880922dbc9a49ccb187808ce3ffcb46
 
-var queryURL = 
+var url = 'https://newsapi.org/v2/everything?' +
+          'q=Environment&' +
+          'from=2019-06-15&' +
+          'sortBy=popularity&' +
+          'apiKey=d880922dbc9a49ccb187808ce3ffcb46';
+
+var req = new Request(url);
+
+  fetch(req)
+    .then(function (response) {
+      console.log(response.json().then(function (a) {
+        console.log(a.articles);
+        var articles = a.articles;
+        var clicks = 0;
+        
+
+        $("#news-btn").click(function() {
+
+        for (var i = 0; i < articles.length; i++) {
+          var link = articles[i].url;
+          var image = articles[i].urlToImage;
+
+          var articleCard = $("<div>");
+          articleCard.addClass("card");
+          articleCard.css({
+            "width": "18rem",
+            "display": "inline-block",
+            "margin": "5px",
+            "border": "1px solid #3E3C39",
+          });
 
 
+          var articleImg = $("<img>");
+          articleImg.addClass("card-img-top");
+          articleImg.attr("src", image);
+          articleCard.append(articleImg);
+
+          var articleHead = $("<h5>");
+          articleHead.addClass("card-title");
+          articleHead.text(articles[i].title);
+          articleCard.append(articleHead);
+
+          var articleLink = $("<a>");
+          articleLink.addClass("btn btn-primary");
+          articleLink.css({
+            "background-color": "#42b63e",
+            "margin-bottom": "5px",
+          });
+          articleLink.attr("href", link);
+          articleLink.text("Go to Article");
+          articleCard.append(articleLink);
+
+          $("#news").append(articleCard);
+
+        };
+  
+
+      });
+      }));
+
+  
+    });
+
+    
 
   $(".map-launch").click(function() {
     $("#map").css("display","inherit");
